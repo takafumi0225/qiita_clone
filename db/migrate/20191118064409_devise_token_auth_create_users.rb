191f9aecd94763cfaaaf1679071cc5abd1162347
貴文 <>
2019-11-18 16:53:05 +0900

    Task.6-1 devise_token_auth を使った user model の作成 - devise_token_auth_create_usersファイルのUser項目修正

--- db/migrate/20191118064409_devise_token_auth_create_users.rb
+++ db/migrate/20191118064409_devise_token_auth_create_users.rb
@@ -30,9 +30,8 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
 
       ## User Info
       t.string :name
-      t.string :nickname
-      t.string :image
       t.string :email
+      t.string :account
 
       ## Tokens
       t.text :tokens
