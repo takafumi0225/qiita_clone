87d1d6bd46b742a351bd3caed01214c08f6f29d6
貴文 <>
2019-11-20 16:45:20 +0900

    Task.6-3 model の validation テストの実装 - validationの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+  # titleは100文字以内の制限をかける
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true
+
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  # パスワードはアルファベットと数字の混合のみ可能にし、文字数制限は8〜32字以内とする
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
+  validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+  # emailは＠と.の使用を必須にする
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+  validates :name, presence: true
+  validates :account, presence: true, uniqueness: true
+
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
