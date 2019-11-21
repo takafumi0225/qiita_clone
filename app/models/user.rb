8aa9419d92053a26add8d58dc8eb87b1862fc788
貴文 <>
2019-11-21 15:45:52 +0900

    Task.6-3 - passwordのvalidation設定変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  # パスワードはアルファベットと数字の混合のみ可能にし、文字数制限は8〜32字以内とする
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
-  validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+  # パスワードはアルファベット半角大文字小文字と数字の混合のみ可能にし、文字数制限は8〜32字以内とする
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX}
   # emailは＠と.の使用を必須にする
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
   validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
