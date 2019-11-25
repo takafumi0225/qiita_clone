4b71ad73904f4114f3e60f95fd4a96428d087f0c
貴文 <>
2019-11-25 17:40:50 +0900

    Task.7-1 bundle exec rubocop -a 実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, { format: 'json' } do
+  namespace :api, { format: "json" } do
     namespace :v1 do # バージョン1を表している
       resources :articles
       mount_devise_token_auth_for "User", at: "auth"
