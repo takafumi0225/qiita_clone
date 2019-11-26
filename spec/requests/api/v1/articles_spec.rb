1cd22a1f552d510c5283c42e6bd64b638d37a63b
貴文 <>
2019-11-26 16:16:34 +0900

    Task.7-3 bundle exec rubocop -a 実行

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,17 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /api/v1/articles" do
     subject { get(api_v1_articles_path) }
+
     before do
       create_list(:article, 3)
     end
+
     it "記事の一覧が取得できる" do
       subject
       res = JSON.parse(response.body)
       expect(res.length).to eq 3
-      expect(res[0].keys).to eq ["id","title","body"]
-      expect(response).to have_http_status(200)
+      expect(res[0].keys).to eq ["id", "title", "body"]
+      expect(response).to have_http_status(:ok)
     end
   end
 end
