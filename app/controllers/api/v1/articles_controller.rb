6d5310c976a4764d931f0e27a6f22229eef76e0c
貴文 <>
2019-11-28 19:04:34 +0900

    Task. 7-5 記事更新の実装/Task. 7-6 記事削除の実装 - API 実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticlesController < Api::V1::BaseApiController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :update, :destroy]
   def index
     articles = Article.all
     render json: articles
@@ -12,6 +12,15 @@ class Api::V1::ArticlesController < Api::V1::BaseApiController
   def create
     article = current_user.articles.create!(article_params)
     render json: article
+  end
+
+  def update
+    @article.update!(article_params)
+    render json: @article
+  end
+
+  def destroy
+    @article.destroy!
   end
 
   private
