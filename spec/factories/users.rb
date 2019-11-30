dbbdf4d5644dc6c90642b108aadd033df9743746
貴文 <>
2019-11-30 15:08:43 +0900

    Task. 7-5/Task. 7-6 パスワードのテストが通らないことがあるため、Fakerの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     sequence(:account) {|n| "#{n}_#{Faker::Internet.username}" }
     sequence(:email) {|n| "#{n}_#{Faker::Internet.email}" }
-    password { Faker::Internet.password(min_length: 8, max_length: 32, mix_case: true) }
+    password { "Ab1#{Faker::Internet.password(min_length: 5, max_length: 28, mix_case: true)}" }
   end
 end
